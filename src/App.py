ef0cc93f7b9b07d3613023d87d1cb17805b20dfa
Denis Sanchez <86267586+DenisV2112@users.noreply.github.com>
2025-05-17 15:56:23 -0500

    Update App.py

--- src/App.py
+++ src/App.py
@@ -4,16 +4,47 @@ from Curriculum import *
 from Update import *
 from Reports import *
 
+
+def check_int(massage) :#Esta función se encarga de verificar que lo ingresado por el usuario sea realmente un número entero para poder convertirlo y agregarlo al valor del producto.
+    entry = check_str(massage)
+    if entry.isdigit():
+        return int(entry)
+    else :
+        print("Dato ingresado invalido!")
+        return check_int(massage)
+
+def check_str(massage):#Esta función verifica que el valor ingresado no esté vacío.
+
+
+    entry = input(massage).strip()
+    if entry == "":
+        print(" no puede estar vacio!")
+        return check_str(massage)
+    else :
+        return entry
+
+
+from colorama import init, Fore, Style
+
+
+
+
+init()
+
+print(f"""
+ ------------------------
+ | {Fore.BLUE} Menú principal {Style.RESET_ALL}     |
+ |----------------------|
+ | {Fore.GREEN} 1. Añadir {Style.RESET_ALL}          |
+ | {Fore.GREEN} 2. Consultar {Style.RESET_ALL}       | 
+ | {Fore.GREEN} 3. Generar Reporte {Style.RESET_ALL} |
+ | {Fore.GREEN} 4. Actualizar {Style.RESET_ALL}      |
+ | {Fore.GREEN} 5. Salir {Style.RESET_ALL}           |
+ ------------------------""")
+
+option = check_int("Ingrese la opcion a realizar: ")
+
 while True:
-    print("\n--- MENÚ PRINCIPAL ---")
-    print("1. Registrar hoja de vida")
-    print("2. Consultar hojas de vida")
-    print("3. Actualizar información")
-    print("4. Generar reportes")
-    print("5. Salir")
-
-    opcion = input("Seleccione una opción: ")
-
     match opcion:
         case '1':
             register()
